e3f027f159990a59c261b6a3a53e152208f1de55
Duarte R <raposoduca@gmail.com>
2022-04-29 15:47:32 +0100

    SMS Activity null pointer exception

--- androidApp/src/main/java/pie/activityrecognition/platform/android/MainActivity.kt
+++ androidApp/src/main/java/pie/activityrecognition/platform/android/MainActivity.kt
@@ -1,49 +1,56 @@
 package pie.activityrecognition.platform.android
 
-import android.app.PendingIntent
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.telephony.SmsManager
-import pie.activityrecognition.platform.Greeting
-import android.widget.TextView
-import com.google.android.gms.location.*
 
-fun greet(): String {
-    return Greeting().greeting()
-}
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.*
+import java.lang.Exception
 
 
 // Alternative lower level Sensor class from Kotlin instead of google's
 // activity recognition API -> https://developer.android.com/reference/kotlin/android/hardware/Sensor
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val tv: TextView = findViewById(R.id.text_view)
-        tv.text = greet()
-        val transitions = mutableListOf<ActivityTransition>()
+        try {
 
-        transitions +=
-            ActivityTransition.Builder()
-                .setActivityType(DetectedActivity.TILTING)
-                .setActivityTransition(ActivityTransition.ACTIVITY_TRANSITION_ENTER)
-                .build()
-        /*
-        transitions +=
-            ActivityTransition.Builder()
-                .setActivityType(DetectedActivity.RUNNING)
-                .setActivityTransition(ActivityTransition.ACTIVITY_TRANSITION_EXIT)
-                .build()
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.SEND_SMS),0)
+            }
 
-        transitions +=
-            ActivityTransition.Builder()
-                .setActivityType(DetectedActivity.WALKING)
-                .setActivityTransition(ActivityTransition.ACTIVITY_TRANSITION_EXIT)
-                .build()
-        */
+            val smsAct = SMSActivity(this.applicationContext)
+            smsAct.sendSMS("934786267", "Testing")
+
+            val transitions = mutableListOf<ActivityTransition>()
+
+            transitions +=
+                ActivityTransition.Builder()
+                    .setActivityType(DetectedActivity.TILTING)
+                    .setActivityTransition(ActivityTransition.ACTIVITY_TRANSITION_ENTER)
+                    .build()
+            /*
+            transitions +=
+                ActivityTransition.Builder()
+                    .setActivityType(DetectedActivity.RUNNING)
+                    .setActivityTransition(ActivityTransition.ACTIVITY_TRANSITION_EXIT)
+                    .build()
+
+            transitions +=
+                ActivityTransition.Builder()
+                    .setActivityType(DetectedActivity.WALKING)
+                    .setActivityTransition(ActivityTransition.ACTIVITY_TRANSITION_EXIT)
+                    .build()
+            */
+        } catch (e: Exception) {
+            println(e)
+        }
     }
 
 }
--- androidApp/src/main/java/pie/activityrecognition/platform/android/SMSActivity.kt
+++ androidApp/src/main/java/pie/activityrecognition/platform/android/SMSActivity.kt
@@ -10,8 +10,9 @@ class SMSActivity(c: Context) {
     private val context = c
     private val smsManager = context.getSystemService(SmsManager::class.java)
 
-    private fun sendSMS(phoneNumber: String, message: String) {
-        val sentPI: PendingIntent = PendingIntent.getBroadcast(context, 0, Intent("SMS_SENT"), 0)
-        smsManager.sendTextMessage(phoneNumber, null, message, sentPI, null)
+    fun sendSMS(phoneNumber: String, message: String) {
+        val sentPI: PendingIntent = PendingIntent.getBroadcast(this.context, 0, Intent("SMS_SENT"), 0)
+        this.smsManager.sendTextMessage(phoneNumber, null, message, sentPI, null)
+        //this.smsManager.sendTextMessage(phoneNumber, null, message, null, null)
     }
 }
